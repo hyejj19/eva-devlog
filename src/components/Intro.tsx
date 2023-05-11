02cada8d29a1c7cd8c0fdecaf6cd74b58191a2b3
Hyejung Park <hyejj19@naver.com>
2023-05-11 14:42:46 +0900

    modify : 프로필 이미지 수정

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,17 +5,21 @@ export default function Intro() {
   return (
     <section className="w-full flex items-center mt-32">
       <article className="flex items-end w-full">
-        <div className="shrink-0 mr-5 w-20 h-20 sm:w-[120px] sm:h-[120px]">
+        <div className="mr-5 w-20 h-20 sm:w-[120px] sm:h-[120px]">
           <Image
             src="/profile_develog.png"
+            alt="프로필 이미지"
             width={120}
             height={120}
-            alt="프로필 이미지"
+            priority
+            quality={100}
           />
         </div>
 
-        <div className="max-w-[400px] text-xs sm:text-base">
-          <p className="font-bold text-lg sm:text-xl mb-3">안녕하세요!🖐️</p>
+        <div className="max-w-[400px] text-sm sm:text-base">
+          <p className="font-bold text-lg sm:text-xl mb-2 sm:mb-3">
+            안녕하세요!🖐️
+          </p>
           <p>기록으로 성장하는 개발자 박혜정 입니다.</p>
           <p>공부하고 느낀 것을 블로그에 기록하고 있어요.</p>
         </div>
