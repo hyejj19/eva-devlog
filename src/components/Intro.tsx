ece1ff4e419b4d15e5cea1560e8e896265f03ae9
Hyejung Park <hyejj19@naver.com>
2023-05-11 14:16:47 +0900

    modify : 반응형 간격, 크기 조절

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,18 +5,21 @@ export default function Intro() {
   return (
     <section className="w-full flex items-center mt-32">
       <article className="flex items-end w-full">
-        <Image
-          src="/profile_develog.png"
-          width={120}
-          height={120}
-          alt="프로필 이미지"
-          className="shrink-0 mr-5"
-        />
-        <div className="max-w-[400px] space-y-2 text-sm md:text-base">
-          <p className="font-bold text-xl">안녕하세요!🖐️</p>
-          <p>기록으로 성장하는 프론트엔드 개발자 박혜정 입니다.</p>
+        <div className="shrink-0 mr-5 w-20 h-20 sm:w-[120px] sm:h-[120px]">
+          <Image
+            src="/profile_develog.png"
+            width={120}
+            height={120}
+            alt="프로필 이미지"
+          />
+        </div>
+
+        <div className="max-w-[400px] text-xs sm:text-base">
+          <p className="font-bold text-lg sm:text-xl mb-3">안녕하세요!🖐️</p>
+          <p>기록으로 성장하는 개발자 박혜정 입니다.</p>
           <p>공부하고 느낀 것을 블로그에 기록하고 있어요.</p>
         </div>
+
         <div className="ml-auto hidden md:block">
           <Link href={'https://friedegg556.tistory.com/'} target="_blank">
             <span className="text-xs text-gray-600 dark:text-gray-300 hover-text">
