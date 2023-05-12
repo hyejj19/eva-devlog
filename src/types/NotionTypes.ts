d935c209e74e7a823f8de45bbb80b3c5a15b8f05
Hyejung Park <hyejj19@naver.com>
2023-05-12 17:02:16 +0900

    chore : 응답 데이터 타입 선언

---
+++ src/types/NotionTypes.ts
@@ -0,0 +1,124 @@
+export interface PostResp {
+  object: string;
+  id: string;
+  created_time: string;
+  last_edited_time: string;
+  created_by: CreatedBy;
+  last_edited_by: LastEditedBy;
+  cover: any;
+  icon: any;
+  parent: Parent;
+  archived: boolean;
+  properties: Properties;
+  url: string;
+}
+
+export interface CreatedBy {
+  object: string;
+  id: string;
+}
+
+export interface LastEditedBy {
+  object: string;
+  id: string;
+}
+
+export interface Parent {
+  type: string;
+  database_id: string;
+}
+
+export interface Properties {
+  Tags: Tags;
+  slug: Slug;
+  active: Active;
+  thumbnail: Thumbnail;
+  published: Published;
+  title: Title;
+}
+
+export interface Tags {
+  id: string;
+  type: string;
+  multi_select: MultiSelect[];
+}
+
+export interface MultiSelect {
+  id: string;
+  name: string;
+  color: string;
+}
+
+export interface Slug {
+  id: string;
+  type: string;
+  rich_text: RichText[];
+}
+
+export interface RichText {
+  type: string;
+  text: Text;
+  annotations: Annotations;
+  plain_text: string;
+  href: any;
+}
+
+export interface Text {
+  content: string;
+  link: any;
+}
+
+export interface Annotations {
+  bold: boolean;
+  italic: boolean;
+  strikethrough: boolean;
+  underline: boolean;
+  code: boolean;
+  color: string;
+}
+
+export interface Active {
+  id: string;
+  type: string;
+  checkbox: boolean;
+}
+
+export interface Thumbnail {
+  id: string;
+  type: string;
+  files: any[];
+}
+
+export interface Published {
+  id: string;
+  type: string;
+  created_time: string;
+}
+
+export interface Title {
+  id: string;
+  type: string;
+  title: Title2[];
+}
+
+export interface Title2 {
+  type: string;
+  text: Text2;
+  annotations: Annotations2;
+  plain_text: string;
+  href: any;
+}
+
+export interface Text2 {
+  content: string;
+  link: any;
+}
+
+export interface Annotations2 {
+  bold: boolean;
+  italic: boolean;
+  strikethrough: boolean;
+  underline: boolean;
+  code: boolean;
+  color: string;
+}
