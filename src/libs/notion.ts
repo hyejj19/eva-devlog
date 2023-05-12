089348f15aa7ccf743b6b1065e663d4ae619d434
Hyejung Park <hyejj19@naver.com>
2023-05-12 17:02:01 +0900

    feat : 게시글 목록 가져오는 api 생성

--- src/libs/notion.ts
+++ src/libs/notion.ts
@@ -1,33 +1,40 @@
 import { Client } from '@notionhq/client';
+import {
+  BlockObjectResponse,
+  PartialBlockObjectResponse,
+} from '@notionhq/client/build/src/api-endpoints';
+
+const checkEnvironment = () => {
+  let base_url =
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'https://example.com';
+
+  return base_url;
+};
 
 const notionClient = new Client({ auth: process.env.NEXT_PUBLIC_NOTION_KEY });
 
-const database_id = process.env.NEXT_PUBLIC_NOTION_DATABASE_ID as string;
-
+// 블로그 목록 가져오기
 export const getPosts = async () => {
-  const response = await notionClient.databases.query({
-    database_id,
-    sorts: [
-      {
-        property: 'published',
-        direction: 'descending',
-      },
-    ],
-  });
+  const response = await fetch(checkEnvironment().concat('/api/posts'));
+  const data = response.json();
 
-  return response.results;
+  return data;
 };
 
+// 블로그 포스트 프로퍼티 가져오기
 export const getPost = async (id: string) => {
   return await notionClient.pages.retrieve({ page_id: id });
 };
 
+// 블로그 포스트 내용 가져오기
 export const getPostContent = async (id: string) => {
   const baseQuery = {
     block_id: id,
     page_size: 100,
   };
-  let results = [];
+  let results: (PartialBlockObjectResponse | BlockObjectResponse)[] = [];
   let postContent = await notionClient.blocks.children.list(baseQuery);
   results = [...postContent.results];
 
