9b2672f1313482f9d3a2f82e9fd5e3c339652b6a
Hyejung Park <hyejj19@naver.com>
2023-05-12 17:40:06 +0900

    feat : 홈페이지 notion api 연동

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,8 +4,6 @@ import TagList from '../../components/TagList';
 import PostList from '../../components/PostList';
 import { getPosts } from '../../libs/notion';
 
-// TODO: Notion API와 연동
-
 export default async function PostsPage() {
   const postInfos = await getPosts();
 
