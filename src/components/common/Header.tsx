33dc5e9d48d43d69d5cfccd8f3fb3bb60a021b13
hyejj19@naver.com <hyejj19@naver.com>
2024-03-08 21:44:15 +0900

    [Modify] Header 불필요한 스크롤 감지 제거

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,24 +1,13 @@
 'use client';
 
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
 import DarkmodeToggle from './DarkmodeToggle';
 
 export default function Header() {
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const updateScroll = () => {
-    setScrollPosition(window.scrollY || document.documentElement.scrollTop);
-  };
-  useEffect(() => {
-    window.addEventListener('scroll', updateScroll);
-  });
-
   return (
     <header
-      className={`flex w-full justify-between pt-5 pb-2 h-[80px] px-4 lg:px-0 items-end fixed max-w-[900px] bg-white dark:bg-deep-gray ${
-        scrollPosition > 40 &&
-        'bg-opacity-80 dark:bg-opacity-80 backdrop-blur-sm'
-      }`}>
+      className={`flex w-full justify-between pt-5 pb-2 h-[80px] px-4 lg:px-0 items-end fixed max-w-[900px] bg-white dark:bg-deep-gray 
+        bg-opacity-80 dark:bg-opacity-80 backdrop-blur-sm`}>
       <Link href="/">
         <div className="cursor-pointer text-3xl font-bold">
           <span className="hover:text-main-orange hover:transition-colors">
