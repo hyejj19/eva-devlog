04e4ccdc69ce742bd4ca1b61e262924b050faa80
hyejj19@naver.com <hyejj19@naver.com>
2024-03-08 22:46:27 +0900

    [Refactor] 태그리스트 Object -> Map 으로 변경해 변수 명시성 개선

--- src/components/common/TagList.tsx
+++ src/components/common/TagList.tsx
@@ -5,18 +5,20 @@ interface TagListProps {
 }
 
 export default function TagList({ tagList }: TagListProps) {
-  const tagsWithAmount: Record<string, number> = tagList.reduce((acc, cur) => {
-    if (!acc[cur]) return { ...acc, [cur]: 1 };
-    return { ...acc, [cur]: acc[cur] + 1 };
-  }, {});
+  const tagsWithAmount = [
+    ...tagList.reduce((acc, cur) => {
+      if (!acc.get(cur)) return acc.set(cur, 1);
+      return acc.set(cur, acc.get(cur) + 1);
+    }, new Map()),
+  ];
 
   return (
     <article className="w-[280px] h-full hidden md:block">
       <h2 className="subtitle">Tags</h2>
       <ul className="space-y-2 text-sm pt-2">
-        {Object.entries(tagsWithAmount).map((tag) => (
-          <li key={tag[0]} className="hover-text">
-            {`${tag[0]} (${tag[1]})`}
+        {tagsWithAmount.map(([tag, amount]) => (
+          <li key={tag} className="hover-text">
+            {`${tag} (${amount})`}
           </li>
         ))}
       </ul>
