12a2f1bb66a399a371b244006be6c5b73de2e441
hyejj19@naver.com <hyejj19@naver.com>
2024-03-15 14:38:34 +0900

    [Feat] 메타 데이터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,33 @@
 import Head from 'next/head';
+import { Metadata } from 'next';
 import React from 'react';
 import Header from '../components/common/Header';
 import '../styles/globals.css';
 import 'highlight.js/styles/tomorrow-night-bright.css';
+
+export const metadata: Metadata = {
+  title: 'Evalog.',
+  description: '이 곳은 제가 공부하고 느낀 것을 기록하는 공간입니다.',
+  viewport: 'width=device-width, initial-scale=1.0',
+  authors: {
+    name: 'eva',
+    url: 'https://github.com/hyejj19',
+  },
+  openGraph: {
+    title: 'Evalog.',
+    siteName: 'Evalog.',
+    type: 'website',
+    url: 'https://evalog.vercel.app/',
+    images: '/profile_devlog.webp',
+  },
+  twitter: {
+    title: 'Evalog.',
+    card: 'summary',
+    description: '이 곳은 제가 공부하고 느낀 것을 기록하는 공간입니다.',
+    images: '/profile_devlog.webp',
+    creator: 'eva',
+  },
+};
 
 export default function RootLayout({
   children,
@@ -16,6 +41,24 @@ export default function RootLayout({
           rel="stylesheet"
           href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/styles/tomorrow-night-bright.min.css"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/highlight.min.js" />
       </Head>
       <body suppressHydrationWarning>
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next';
 import ContentLayout from '../../components/post/ContentLayout';
 import { Education } from '../../components/resume/\bEducation';
 import { Career } from '../../components/resume/Career';
@@ -5,6 +6,30 @@ import { Intro } from '../../components/resume/Intro';
 import { Projects } from '../../components/resume/Projects';
 import { SoftSkills } from '../../components/resume/SoftSkills';
 import { Stacks } from '../../components/resume/Stacks';
+
+export const metadata: Metadata = {
+  title: '박혜정 이력서',
+  description: '프론트엔드 개발자 박혜정 입니다.',
+  viewport: 'width=device-width, initial-scale=1.0',
+  authors: {
+    name: 'eva',
+    url: 'https://github.com/hyejj19',
+  },
+  openGraph: {
+    title: '박혜정 이력서.',
+    siteName: 'Eva박혜정 이력서log.',
+    type: 'website',
+    url: 'https://evalog.vercel.app/resume',
+    images: '/profile_devlog.webp',
+  },
+  twitter: {
+    title: '박혜정 이력서.',
+    card: 'summary',
+    description: '프론트엔드 개발자 박혜정 입니다.',
+    images: '/profile_devlog.webp',
+    creator: 'eva',
+  },
+};
 
 export default function Resume() {
   return (
