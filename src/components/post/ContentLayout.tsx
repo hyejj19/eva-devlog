fc3be46489467ad8d4b7c257ebfdf3c5ead7a66b
hyejj19@naver.com <hyejj19@naver.com>
2024-03-30 18:02:14 +0900

    [Modify] TOC 와 본문 사이 간격 추가

--- src/components/post/ContentLayout.tsx
+++ src/components/post/ContentLayout.tsx
@@ -2,7 +2,7 @@
 
 export default function ContentLayout({ children }) {
   return (
-    <section className="flex w-full h-full justify-between pt-20">
+    <section className="flex w-full h-full justify-between pt-20 md:space-x-5">
       {children}
     </section>
   );
