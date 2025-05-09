97caab8502fd4a3c6fe30b3cf8467975f5a4ce9f
hyejj19@naver.com <hyejj19@naver.com>
2025-05-09 17:08:16 +0900

    feat: meta data

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,7 +1,31 @@
+import { Metadata } from 'next';
 import React from 'react';
 import ContentLayout from '../../../components/post/ContentLayout';
 import PostContent from '../../../components/post/PostContent';
 import { getArticleData } from '../../../utils/articles-utils';
+
+export async function generateMetadata({ params }): Promise<Metadata> {
+  const { id: slug } = params;
+  const articleData = getArticleData(slug);
+
+  return {
+    title: articleData.title,
+    description: articleData.excerpt,
+    openGraph: {
+      title: articleData.title,
+      description: articleData.excerpt,
+      type: 'article',
+      publishedTime: articleData.date,
+      images: [articleData.image],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: articleData.title,
+      description: articleData.excerpt,
+      images: [articleData.image],
+    },
+  };
+}
 
 export default async function PostPage({ params }) {
   const { id: slug } = params;
